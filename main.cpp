56b701e8eadad07a2c1dd94400c11a86ef062d22
khanatal92 <akhan126@insite.4cd.edu>
2024-03-11 15:05:52 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int prev, current;
-	
 	cin >> prev;
+	cout << prev; 
+	
 
-	while (cin >> current)
-	{
-		if (current <= prev)
-	{
-		cout << current << " ";
+	while (true) {
+		cin >> current;
+		if (current > prev){
+			break;
+		} else if (current != prev)
+		{
+			cout << ' ';
+			cout << current;
+			current = prev;
+		}
 	}
-	else
-	{
-	cout << endl;
-	break;
-	}
-	}
-
 	return 0;
 }
